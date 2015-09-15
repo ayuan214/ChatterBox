ec82a46608c889795e17265a137664588b49d3ab
ayuan214 <alvinyuan@gmail.com>
2015-09-15 01:10:22 -0400

    Revert "feat: comments fixed"
    This reverts commit 2ed50f341453e1ce50066519ed1aa82ab0b5b6fc.

--- routes/index.js
+++ routes/index.js
@@ -34,12 +34,8 @@ router.param('post', function(req, res, next, id) {
   });
 });
 
-router.get('/posts/:post', function(req, res, next) {
-  req.post.populate('comments', function(err, post) {
-    if (err) { return next(err); }
-
-    res.json(post);
-  });
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
 });
 
 router.put('/posts/:post/upvote', function(req, res, next) {
@@ -66,28 +62,14 @@ router.post('/posts/:post/comments', function(req, res, next) {
   });
 });
 
-router.param('comment', function(req, res, next, id) {
-  var query = Comment.findById(id);
-
-  query.exec(function (err, comment){
-    if (err) { return next(err); }
-    if (!comment) { return next(new Error('can\'t find comment')); }
-
-    req.comment = comment;
-    return next();
-  });
-});
-
-
 router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
-  req.comment.upvote(function(err, post){
+  req.post.upvote(function(err, post){
     if (err) { return next(err); }
 
     res.json(post);
   });
 });
 
-/*
 router.get('/posts/:post', function(req, res, next) {
   req.post.populate('comments', function(err, post) {
     if (err) { return next(err); }
@@ -95,7 +77,6 @@ router.get('/posts/:post', function(req, res, next) {
     res.json(post);
   });
 });
-*/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
