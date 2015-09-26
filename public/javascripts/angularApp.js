0a53182348af7f229bd3a0b48de39e90299fb4b2
ayuan214 <alvinyuan@gmail.com>
2015-09-26 01:59:42 -0700

    feat: Comments and Posts Login
    Comments and Posts must require login

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -53,7 +53,7 @@ function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('home');
 }]);
 
-app.factory('posts', ['$http', function($http){
+app.factory('posts', ['$http', 'auth', function($http, auth){
 	var o = {
 		posts: []
 	};
@@ -63,29 +63,33 @@ app.factory('posts', ['$http', function($http){
 	    });
 	};
 	o.create = function(post) {
-		return $http.post('/posts', post).success(function(data){
-			o.posts.push(data);
-		});
+	  return $http.post('/posts', post, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  }).success(function(data){
+	    o.posts.push(data);
+	  });
 	};
+
 	o.upvote = function(post) {
-		return $http.put('/posts/' + post._id + '/upvote')
-			.success(function(data){
-	     	post.upvotes += 1;
-	    });
+	  return $http.put('/posts/' + post._id + '/upvote', null, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  }).success(function(data){
+	    post.upvotes += 1;
+	  });
 	};
-	o.get = function(id) {
-		return $http.get('/posts/' + id).then(function(res){
-			return res.data;
-		});
-	};
+
 	o.addComment = function(id, comment) {
-	  return $http.post('/posts/' + id + '/comments', comment);
+	  return $http.post('/posts/' + id + '/comments', comment, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  });
 	};
+
 	o.upvoteComment = function(post, comment) {
-		return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
-	    .success(function(data){
-	      	comment.upvotes += 1;
-    	});
+	  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  }).success(function(data){
+	    comment.upvotes += 1;
+	  });
 	};
   return o;
 }])
@@ -135,9 +139,10 @@ app.factory('auth', ['$http', '$window', function($http, $window){
 }])
 
 app.controller('MainCtrl', [
-'$scope', 'posts',
-function($scope, posts){
+'$scope', 'posts', 'auth',
+function($scope, posts, auth){
 	$scope.test = 'Hello world!';
+	$scope.isLoggedIn = auth.isLoggedIn;
 	$scope.posts = posts.posts;
 	$scope.addPost = function(){
 	  if(!$scope.title || $scope.title === '') { return; }
@@ -158,9 +163,11 @@ app.controller('PostsCtrl', [
 	'$scope',
 	'posts',
 	'post',
+	'auth',
 
-	function($scope, posts, post){
+	function($scope, posts, post, auth){
 		$scope.post = post;
+		$scope.isLoggedIn = auth.isLoggedIn;
 		$scope.addComment = function(){
 			if($scope.body === '') { return; }
 			posts.addComment(post._id, {
