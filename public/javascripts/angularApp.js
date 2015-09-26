18eb05c636f73aa0c920edf401be04030bd4130c
ayuan214 <alvinyuan@gmail.com>
2015-09-26 01:43:01 -0700

    feat: Updated Navi Bar

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -201,3 +201,12 @@ function($scope, $state, auth){
     });
   };
 }])
+
+app.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth){
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
+}]);
